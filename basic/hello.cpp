171bafe5838d6fe4d559eb96f89842624b3b4300
Duc Cuong <duccuong.hust@gmail.com>
2015-07-10 12:47:24 +0800

    First commit

---
+++ basic/hello.cpp
@@ -0,0 +1,14 @@
+/**
+ * First program in C++
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+ {
+ 	cout << "Hello World!" << endl;
+ 	/* code */
+ 	return 0;
+ }
+
+ // RUN: successfully
